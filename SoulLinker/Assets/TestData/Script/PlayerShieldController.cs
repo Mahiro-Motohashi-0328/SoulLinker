8dc58c052d07b46009dba4bd55ac182ad921eb15
mahiroPoketto <mahiro.motohashi@icloud.com>
2020-09-17 11:41:44 +0900

    バトル用スクリプト基礎の完成（改善の余地あり）

--- SoulLinker/Assets/TestData/Script/PlayerShieldController.cs
+++ SoulLinker/Assets/TestData/Script/PlayerShieldController.cs
@@ -4,29 +4,77 @@ using UnityEngine;
 
 public class PlayerShieldController : MonoBehaviour
 {
-    public int speed = 5;
-    // Start is called before the first frame update
-    void Start()
-    {
+    [SerializeField] GameObject ball;
+    [SerializeField] GameObject PlayerShield;
+    [SerializeField] GameObject PlayerSoul;
+    [SerializeField] GameObject enemySoul;
+    [SerializeField] int speed = 5;
+    [SerializeField] public int HitCount = 0;
+    [SerializeField] public bool death = false;
+    Rigidbody2D Ball;
+    Rigidbody2D Shield;
+    Rigidbody Player;
+    Rigidbody enemy;
+    Collider2D ballCollider;
+    Collider2D pSoul;
+    Collider2D eSoul;
 
+    /// <summary>
+    /// 初期化処理。SetActiveがTrueになるたび呼び出される
+    /// </summary>
+    private void OnEnable()
+    {
+        //死亡判定のリセット
+        death = false;
+        //Hit回数の初期化
+        HitCount = 0;
+
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //アタックボールの制御
+        Ball = ball.GetComponent<Rigidbody2D>();
+        //プレイヤー動作
+        Shield = PlayerShield.GetComponent<Rigidbody2D>();
+        ballCollider = ball.GetComponent<Collider2D>();
+        pSoul = PlayerSoul.GetComponent<Collider2D>();
+        eSoul = enemySoul.GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //プレイヤー動作
-        Rigidbody2D body = GetComponent<Rigidbody2D>();
-        body.velocity = Vector2.zero;
+        if (ballCollider.IsTouching(eSoul))
+        {
+            HitCount++;
+        }
+
+        if (ballCollider.IsTouching(pSoul))
+        {
+            death = true;
+        }
+
+    }
+    /// <summary>
+    /// プレイヤーの盾を動かす
+    /// </summary>
+    public void movementShield()
+    {
+        Shield.velocity = Vector2.zero;
         if (Input.GetKey(KeyCode.UpArrow))
         {
             //transform.position += Vector3.left * speed;
-            body.velocity = Vector2.up * speed;
+            Shield.velocity = Vector2.up * speed;
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
             //transform.position += Vector3.right * speed;
-            body.velocity = Vector2.down * speed;
+            Shield.velocity = Vector2.down * speed;
         }
-
     }
+
+
+
 }
