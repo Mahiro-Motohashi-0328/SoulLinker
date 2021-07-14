b4a542ed1ee94d687f7e10344dffb73e53bd6cef
mahiroPoketto <mahiro.motohashi@icloud.com>
2021-07-15 01:07:20 +0900

    アクション要素の追加

---
+++ SoulLinker/Assets/TestData/Script/PlayerMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private float m_speed;
+    private Rigidbody2D m_rigidbody2D = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //キー入力されたら行動する
+        float horizontalKey = Input.GetAxis("Horizontal");
+        float xSpeed = 0.0f;
+        if (horizontalKey > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            xSpeed = m_speed;
+        }
+        else if (horizontalKey < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            xSpeed = -m_speed;
+        }
+        else
+        {
+            xSpeed = 0.0f;
+        }
+        m_rigidbody2D.velocity = new Vector2(xSpeed, m_rigidbody2D.velocity.y);
+    }
+}
